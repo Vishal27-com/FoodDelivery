70c6246f85667540d69937df050597f95feb2474
Vishal27-com <vishalgupta962148@gmail.com>
2023-06-03 16:38:20 +0530

    db connected

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+require("dotenv").config();
 const express = require('express')
 const app = express()
 const cors=requiree("cors");
+const dbConnect=require("./Db/db")
 const userRouter=require("./Routes/user.routes")
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors());
 app.get('/', (req, res) => res.send('hello'))
 app.use('/api',userRouter)
-app.listen(8080, () => {console.log('server started on port 8080')})
+app.listen(8080, () => {
+    dbConnect()
+    console.log('server started on port 8080')
+})
